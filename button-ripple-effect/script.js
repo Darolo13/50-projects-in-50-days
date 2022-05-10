18c925d5a27ace83cbfb72f86111737463af98bf
Darolo13 <davidromero1244@gmail.com>
2022-05-10 00:18:33 -0700

    Style ripple effect

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -5,7 +5,7 @@ buttons.forEach(button => {
     const x = e.clientX;
     const y = e.clientY;
 
-    const buttonTop = e.target.offestTop;
+    const buttonTop = e.target.offsetTop;
     const buttonLeft = e.target.offsetLeft;
 
     const xInside = x - buttonLeft;
